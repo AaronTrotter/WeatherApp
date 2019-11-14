d885f84973d5ee11882f0f15910b1a208110c874
Aaron Trotter <aaronjtrotter@gmail.com>
2019-11-14 14:23:58 +0000

    remove firestore stuff

--- server.js
+++ server.js
@@ -4,32 +4,23 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const apiKey = '0cb283b0d1133d96666eab4166fb3fc1';
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
-// app.get('/', function (req, res) {
-//     res.send('Hello World!')
-// })
-
 app.get('/', function (req, res) {
-    // NEW CODE
-    res.render('index');
-})
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    res.render('index', { weather: null, error: null });
 })
 
 app.post('/', function (req, res) {
     let city = req.body.city;
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
     request(url, function (err, response, body) {
         if (err) {
             res.render('index', { weather: null, error: 'Error, please try again' });
         } else {
-            let weather = JSON.parse(body);
-            console.log(body);
+            let weather = JSON.parse(body)
             if (weather.main == undefined) {
                 res.render('index', { weather: null, error: 'Error, please try again' });
             } else {
@@ -38,6 +29,10 @@ app.post('/', function (req, res) {
             }
         }
     });
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
 })
 
 //https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
