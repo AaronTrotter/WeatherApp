efe1399d71ace3ef0e61cbfd4cc838364edac20c
Aaron Trotter <aaronjtrotter@gmail.com>
2019-11-14 14:05:36 +0000

    metric

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.listen(3000, function () {
 
 app.post('/', function (req, res) {
     let city = req.body.city;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
     request(url, function (err, response, body) {
         if (err) {
             res.render('index', { weather: null, error: 'Error, please try again' });
